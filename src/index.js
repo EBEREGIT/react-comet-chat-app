a3f4eed4b72fc28d3e5388392d45e5e814bfe5ab
EBEREGIT <samsoneberenjoku@gmail.com>
2021-02-24 13:42:07 +0100

    demo app with CometChat

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { CometChat } from "@cometchat-pro/chat";
+
+const appID = "296897a36bd79d6";
+const region = "us";
+const appSetting = new CometChat.AppSettingsBuilder()
+  .subscribePresenceForAllUsers()
+  .setRegion(region)
+  .build();
+CometChat.init(appID, appSetting).then(
+  () => {
+    console.log("Initialization completed successfully");
+    // You can now call login function.
+    const authKey = "c531237bf8a2fb109ffd2e7df0097dc14963768b";
+    const uid = "SUPERHERO1";
+
+    CometChat.login(uid, authKey).then(
+      (user) => {
+        console.log("Login Successful:", { user });
+      },
+      (error) => {
+        console.log("Login failed with exception:", { error });
+      }
+    );
+  },
+  (error) => {
+    console.log("Initialization failed with error:", error);
+    // Check the reason for error and take appropriate action.
+  }
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
